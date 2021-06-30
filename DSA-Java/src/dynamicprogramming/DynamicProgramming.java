571155258297c36a8a3c3a6d6a40feafca0f11bd
N-Sridhar <nsridharbtech@gmail.com>
2021-06-30 12:07:48 +0530

    Equals - DP

--- DSA-Java/src/dynamicprogramming/DynamicProgramming.java
+++ DSA-Java/src/dynamicprogramming/DynamicProgramming.java
@@ -1,7 +1,11 @@
 package dynamicprogramming;
 
+import java.util.*;
+
 public class DynamicProgramming {
 	/**
+	 * Coin Change.
+	 * 
 	 * @param amount: 3
 	 * @param coins:  {8, 3, 1, 2}
 	 * 
@@ -25,8 +29,38 @@ public class DynamicProgramming {
 		return combinations[amount];
 	}
 
+	/**
+	 * Chocolate Equal Distribution at minimal steps.
+	 * 
+	 * @param current distribution [2, 2, 3, 7]
+	 * 
+	 * @return minimum steps: 2
+	 * 
+	 *         1, 2 and 5 can be distributes at a time except 1.
+	 * 
+	 *         Add 1 to all but not 3rd -> [3, 3, 3, 8] Add 5 to all but not 4th ->
+	 *         [8, 8, 8, 8]
+	 * 
+	 */
+	static int equals(int[] arr) {
+
+		int[] pos = new int[5];
+
+		int min = Arrays.stream(arr).min().getAsInt();
+
+		for (int i = 0; i < pos.length; i++) {
+			for (int a : arr) {
+				int diff = a - min;
+				pos[i] += diff / 5 + (diff % 5) / 2 + ((diff % 5) % 2) / 1;
+			}
+			min--;
+		}
+		return Arrays.stream(pos).min().getAsInt();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Coin Change: " + coinChange(3, new int[] { 8, 3, 1, 2 }));
+		System.out.println("Equals: " + equals(new int[] { 2, 2, 3, 7 }));
 	}
 
 }
