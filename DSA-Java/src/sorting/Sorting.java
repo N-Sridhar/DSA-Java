5fd58afe137bba91794eb86fa6f9ea65f279e832
N-Sridhar <nsridharbtech@gmail.com>
2021-10-11 23:25:51 +0530

    Bubble Sort

--- DSA-Java/src/sorting/Sorting.java
+++ DSA-Java/src/sorting/Sorting.java
@@ -5,6 +5,8 @@ public class Sorting {
 	public static void main(String[] args) {
 		int[] arr = { 5, 2, 1, 3, 4 };
 
+		bubbleSort(arr);
+
 //		insertionSort(arr);
 
 //		selectionSort(arr);
@@ -14,6 +16,19 @@ public class Sorting {
 //		mergeSort(arr, 0, arr.length - 1);
 
 		arrayPrinter(arr);
+	}
+
+	static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
 	}
 
 	static void insertionSort(int[] arr) {
